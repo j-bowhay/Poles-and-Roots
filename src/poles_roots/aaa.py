fix off by one bug

src/poles_roots/aaa.py
@@ -107,7 +107,7 @@ def AAA(F, Z, *, tol=1e-13, mmax=100, cleanup=True, cleanup_tol=1e-13) -> AAARes
 
         # Compute weights:
         # The usual tall-skinny case
-        if J.size >= m:
+        if J.size >= m + 1:
             # Reduced SVD
             _, s, V = scipy.linalg.svd(A[J, : m + 1], full_matrices=False)
             # Treat case of multiple min sing val