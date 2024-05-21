28c2779d8db97bd10de00ed9f52b94c599822c11
Jake Bowhay <jb9.bowhay@gmail.com>
2024-05-21 22:48:17 +0100

    format plotting file

--- src/poles_roots/plotting.py
+++ src/poles_roots/plotting.py
@@ -1,22 +1,25 @@
 import numpy as np
 
+
 def phase_plot(f, ax, /, *, domain=None, classic=False, n_points=500):
     theta = -np.pi
     domain = [-1, 1, -1, 1] if domain is None else domain
-    
+
     x = np.linspace(domain[0], domain[1], num=n_points)
     y = np.linspace(domain[2], domain[3], num=n_points)
-    
+
     [xx, yy] = np.meshgrid(x, y)
-    zz = xx + yy*1j
-    
+    zz = xx + yy * 1j
+
     if classic:
         phi = lambda t: t
     else:
-        phi = lambda t:  t - .5*np.cos(1.5*t)**3*np.sin(1.5*t)
-    
-    angle = np.mod(phi( np.angle(f(zz)) - np.pi)+ np.pi - theta, 2*np.pi) + theta
-    im = ax.pcolormesh(np.real(zz), np.imag(zz), angle, shading='gouraud', cmap="twilight_shifted")
+        phi = lambda t: t - 0.5 * np.cos(1.5 * t) ** 3 * np.sin(1.5 * t)
+
+    angle = np.mod(phi(np.angle(f(zz)) - np.pi) + np.pi - theta, 2 * np.pi) + theta
+    im = ax.pcolormesh(
+        np.real(zz), np.imag(zz), angle, shading="gouraud", cmap="twilight_shifted"
+    )
     ax.set_xlabel(r"$\Re$(z)")
     ax.set_ylabel(r"$\Im$(z)")
     return im
