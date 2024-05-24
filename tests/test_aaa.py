499751d20600a825b3fd722f2309650ad251d601
Jake Bowhay <jb9.bowhay@gmail.com>
2024-05-24 11:22:09 +0100

    make tests more pythonic

--- tests/test_aaa.py
+++ tests/test_aaa.py
@@ -1,6 +1,7 @@
 import numpy as np
+from numpy.testing import assert_allclose, assert_equal, assert_array_less
 import scipy
-import scipy.special
+
 from poles_roots.aaa import AAA
 
 TOL = 1e4 * np.finfo(np.float64).eps
@@ -12,8 +13,8 @@ class TestAAA:
         F = np.exp(Z)
         r = AAA(F, Z)
 
-        assert np.linalg.norm(F - r(Z), np.inf) < TOL
-        assert np.isnan(r(np.nan))
+        assert_allclose(r(Z), F, atol=TOL)
+        assert_equal(r(np.nan), np.nan)
         assert np.isfinite(r(np.inf))
 
         m1 = r.zj.size
@@ -29,9 +30,9 @@ class TestAAA:
         F = np.tan(np.pi * Z)
         r = AAA(F, Z)
 
-        assert np.linalg.norm(F - r(Z), np.inf) < 10 * TOL
-        assert np.min(np.abs(r.zer)) < TOL
-        assert np.min(np.abs(r.pol - 0.5)) < TOL
+        assert_allclose(r(Z), F, atol=10 * TOL)
+        assert_array_less(np.min(np.abs(r.zer)), TOL)
+        assert_array_less(np.min(np.abs(r.pol - 0.5)), TOL)
         # Test for spurious poles
         assert np.min(np.abs(r.res)) > 1e-13
 
@@ -39,12 +40,12 @@ class TestAAA:
         Z = [0, 1]
         F = [1, 2]
         r = AAA(F, Z)
-        assert np.linalg.norm(F - r(Z), np.inf) < TOL
+        assert_allclose(r(Z), F, atol=TOL)
 
         Z = [0, 1, 2]
         F = [1, 0, 0]
         r = AAA(F, Z)
-        assert np.linalg.norm(F - r(Z), np.inf) < TOL
+        assert_allclose(r(Z), F, atol=TOL)
 
     def test_scale_invariance(self):
         Z = np.linspace(0.3, 1.5)
@@ -52,8 +53,8 @@ class TestAAA:
         r1 = AAA(F, Z)
         r2 = AAA(2**3 * F, Z)
         r3 = AAA(2**-3 * F, Z)
-        assert r1(0.2j) == 2**-3 * r2(0.2j)
-        assert r1(1.4) == 2**3 * r3(1.4)
+        assert_equal(r1(0.2j), 2**-3 * r2(0.2j))
+        assert_equal(r1(1.4), 2**3 * r3(1.4))
 
     # Enable if / when auto Z is added
     # def test_gamma(self):
@@ -68,25 +69,25 @@ class TestAAA:
             return np.log(5 - z) / (1 + z**2)
 
         r = AAA(f(Z), Z)
-        assert np.abs(r(0) - f(0)) < TOL
+        assert_allclose(r(0), f(0), atol=TOL)
 
     def test_infinite_data(self):
         Z = np.linspace(-1, 1)
         r = AAA(scipy.special.gamma(Z), Z)
-        assert np.abs(r(0.63) - scipy.special.gamma(0.63)) < 1e-3
+        assert_allclose(r(0.63), scipy.special.gamma(0.63), atol=1e-15)
 
     def test_nan(self):
         X = np.linspace(0, 20)
         F = np.sin(X) / X
         r = AAA(F, X)
-        assert abs(r(2) - np.sin(2) / 2) < 1e-3
+        assert_allclose(r(2), np.sin(2) / 2, atol=1e-15)
 
     def test_residues(self):
         X = np.linspace(-1.337, 2, num=537)
         r = AAA(np.exp(X) / X, X)
         ii = np.nonzero(np.abs(r.pol) < 1e-8)[0]
-        assert np.abs(r.res[ii] - 1) < 1e-10
+        assert_allclose(r.res[ii], 1, atol=1e-15)
 
         r = AAA((1 + 1j) * scipy.special.gamma(X), X)
         ii = np.nonzero(abs(r.pol - (-1)) < 1e-8)
-        assert np.abs(r.res[ii] + (1 + 1j)) < 1e-10
+        assert_allclose(r.res[ii], -1 - 1j, atol=1e-15)
