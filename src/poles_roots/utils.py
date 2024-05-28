d1d112af7a83ce081b96d328e80f27217bf3f19b
Jake Bowhay <jb9.bowhay@gmail.com>
2024-05-28 20:18:55 +0100

    add function for computing if a point is in a triangle

--- src/poles_roots/utils.py
+++ src/poles_roots/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 
 def convert_cart_to_complex(points):
@@ -15,3 +16,19 @@ def parametrise_between_two_points(a, b):
         return a * (1 - t) + b * t
 
     return param, b - a
+
+
+def point_in_triangle(point, A, B, C):
+    """Check if a point lies in the triangle ABC"""
+    # TODO: vectorise
+    p = point - A
+    c = C - A
+    b = B - A
+
+    lhs = np.array([[np.dot(c, c), np.dot(b, c)], [np.dot(b, c), np.dot(b, b)]])
+
+    rhs = np.array([np.dot(p, c), np.dot(p, b)])
+
+    u, v = scipy.linalg.solve(lhs, rhs)
+
+    return u > 0 and v > 0 and 1 - u - v > 0
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,11 @@
 import numpy as np
 from numpy.testing import assert_equal
 
-from poles_roots.utils import convert_cart_to_complex, parametrise_between_two_points
+from poles_roots.utils import (
+    convert_cart_to_complex,
+    parametrise_between_two_points,
+    point_in_triangle,
+)
 
 
 def test_convert_cart_to_complex():
@@ -18,3 +22,15 @@ def test_parametrise_between_two_points():
 
     assert_equal(param(np.array([0, 0.5, 1])), [0, 0.5 + 0.5j, 1 + 1j])
     assert_equal(jac, 1 + 1j)
+
+
+def test_point_in_triangle():
+    assert point_in_triangle(
+        np.array([0.1, 0.1]), np.array([0, 0]), np.array([1, 0]), np.array([0, 1])
+    )
+    assert not point_in_triangle(
+        np.array([-0.1, 0.1]), np.array([0, 0]), np.array([1, 0]), np.array([0, 1])
+    )
+    assert not point_in_triangle(
+        np.array([1, 1]), np.array([0, 0]), np.array([1, 0]), np.array([0, 1])
+    )
