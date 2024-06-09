c897ca696f3a2bb0a93514eb29e0e19fec34623a
Jake Bowhay <jb9.bowhay@gmail.com>
2024-06-09 18:02:26 +0100

    niave insertion of points for handling singulatrity

--- src/poles_roots/triangulation.py
+++ src/poles_roots/triangulation.py
@@ -15,7 +15,11 @@ def adaptive_triangulation(
     arg_principal_threshold,
     plot=False,
     quad_kwargs: Optional[dict] = None,
+    rng=None,
 ):
+    if rng is None:
+        rng = np.random.default_rng()
+
     initial_points = np.asarray(initial_points)
     points = np.column_stack([initial_points.real, initial_points.imag])
     tri = scipy.spatial.Delaunay(points)
@@ -27,7 +31,9 @@ def adaptive_triangulation(
         quad_kwargs,
     )
 
-    while to_insert := np.any(np.abs(z_minus_p) > arg_principal_threshold):
+    while (
+        np.any(to_insert := np.abs(z_minus_p) > arg_principal_threshold) or to_destroy
+    ):
         if plot:
             fig, ax = plt.subplots()
             plot_triangulation_with_argument_principle(
@@ -40,12 +46,27 @@ def adaptive_triangulation(
             )
             plt.show()
 
-        # compute the points to be added to the triangulation
-        insert_index = np.nonzero(to_insert)[0]
-        points_to_add = np.empty((insert_index.size, 2))
-        for i, simplex in enumerate(tri.simplices[insert_index]):
-            A, B, C = tri.points[simplex, :]
-            points_to_add[i, :] = compute_incenter(A, B, C)
+        if to_destroy:
+            # destroy edge if we are integrating through a singularity
+            points_to_add = np.empty((len(to_destroy), 2))
+            for i, (a, b) in enumerate(to_destroy):
+                center = (a + b) / 2
+                radius = np.abs(a - b) / 2
+                r = radius * np.sqrt(rng.random())
+                theta = rng.uniform(0, 2 * np.pi)
+                new = center + r * np.exp(theta * 1j)
+                points_to_add[i, :] = [new.real, new.imag]
+
+        else:
+            # compute the points to be added to the triangulation
+            insert_index = np.flatnonzero(to_insert)
+            points_to_add = np.empty((insert_index.size, 2))
+            # destroy triangles where |Z-P| is too large
+            for i, simplex in enumerate(tri.simplices[insert_index]):
+                A, B, C = tri.points[simplex, :]
+                points_to_add[i, :] = compute_incenter(A, B, C)
+
+        # add new points to triangulation
         points = np.concatenate([points, points_to_add])
         tri = scipy.spatial.Delaunay(points)
 
