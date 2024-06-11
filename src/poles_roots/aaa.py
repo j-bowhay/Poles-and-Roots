3d824468a007ad9c7ed2799b34dfde999db21ccb
Jake Bowhay <jb9.bowhay@gmail.com>
2024-06-11 23:02:34 +0100

    disable svd finite check

--- src/poles_roots/aaa.py
+++ src/poles_roots/aaa.py
@@ -113,7 +113,9 @@ def AAA(F, Z, *, tol=1e-13, mmax=100, cleanup=True, cleanup_tol=1e-13) -> _AAARe
         # The usual tall-skinny case
         if mask.sum() >= m + 1:
             # Reduced SVD
-            _, s, V = scipy.linalg.svd(A[mask, : m + 1], full_matrices=False)
+            _, s, V = scipy.linalg.svd(
+                A[mask, : m + 1], full_matrices=False, check_finite=False
+            )
             # Treat case of multiple min sing val
             mm = np.nonzero(s == np.min(s))[0]
             nm = mm.size
