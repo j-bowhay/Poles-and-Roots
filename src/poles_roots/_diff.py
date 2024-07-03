364add19b4d936c5d50d93030fbb7f54f14725f4
Jake Bowhay <jb9.bowhay@gmail.com>
2024-07-03 22:36:05 +0100

    basic complex derivative working

--- src/poles_roots/_diff.py
+++ src/poles_roots/_diff.py
@@ -6,3 +6,10 @@ h = np.sqrt(np.finfo(np.float64).eps)
 def derivative(f, z, /):
     """Forwards difference."""
     return (f(z + h) - f(z)) / h
+
+
+def complex_derivitive(f, z, N):
+    k = np.arange(N)
+    zk = np.exp(2 * np.pi * 1j * k / N)
+    fk = f(zk)
+    return np.mean(zk * fk / (z - zk) ** 2)
