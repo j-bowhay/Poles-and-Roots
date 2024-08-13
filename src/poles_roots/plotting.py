242bf38662747044fd7d5ec614cc063f5faa417d
Jake Bowhay <jb9.bowhay@gmail.com>
2024-08-13 15:22:14 +0100

    better plotting

--- src/poles_roots/plotting.py
+++ src/poles_roots/plotting.py
@@ -7,15 +7,15 @@ from colorsys import hls_to_rgb
 
 def colorize(z):
     r = np.abs(z)
-    arg = np.angle(z) 
+    arg = np.angle(z)
 
-    h = (arg + np.pi)  / (2 * np.pi) + 0.5
-    l = 1.0 - 1.0/(1.0 + r**0.3)
+    h = (arg + np.pi) / (2 * np.pi) + 0.5
+    l = 0.5  # 1.0 - 1.0 / (1.0 + (r/r.max())**0.005)
     s = 0.8
 
-    c = np.vectorize(hls_to_rgb) (h,l,s) # --> tuple
+    c = np.vectorize(hls_to_rgb)(h, l, s)  # --> tuple
     c = np.array(c)  # -->  array of (3,n,m) shape, but need (n,m,3)
-    c = c.transpose(1,2,0)
+    c = c.transpose(1, 2, 0)
     return c
 
 
